7dbb3edb1e7c1e514ece2df2132a65597694cf36
Nemo HunJae Lee <nemo.hj.lee@gmail.com>
2013-09-21 11:54:30 +0900

    Added command event

---
+++ command.thrift
@@ -0,0 +1,17 @@
+namespace java fresto.command
+
+enum TargetModule {
+    CLIENT_EVENT_HUB = 1,
+    SERVER_EVENT_HUB = 2,
+    CLIENT_EVENT_WRITER = 3,
+    SERVER_EVENT_WRITER = 4
+}
+
+enum Command {
+    TERMINATE = 1
+}
+
+struct CommandEvent {
+    1: required TargetModule target_module;
+    2: required Command	command;
+}
